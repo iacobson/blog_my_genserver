5c5f7e5f0cd18eba4791145abbfd887ce0f29f0e
IacobSon <dorian.iacobescu@gmail.com>
2018-05-27 17:18:54 +0200

    finish example

--- lib/example.ex
+++ lib/example.ex
@@ -1,48 +1,86 @@
 defmodule Example do
+  # API
+
   def start_link() do
     MyGenserver.start_link(__MODULE__, :ok)
   end
 
-  def expensive(pid, num) do
-    MyGenserver.cast(pid, {:expensive, num})
+  def expensive_call(pid, num, order) do
+    IO.inspect(order, label: "REQUEST ORDER: ")
+    MyGenserver.call(pid, {:expensive, num, order})
   end
 
-  def light(pid, num) do
-    MyGenserver.cast(pid, {:light, num})
+  def light_call(pid, num, order) do
+    IO.inspect(order, label: "REQUEST ORDER: ")
+    MyGenserver.call(pid, {:light, num, order})
   end
 
-  def show(pid) do
-    MyGenserver.call(pid, :show)
+  def expensive_cast(pid, num, order) do
+    IO.inspect(order, label: "REQUEST ORDER: ")
+    MyGenserver.cast(pid, {:expensive, num, order})
   end
 
-  def stop_server(pid) do
-    MyGenserver.stop(pid, :other)
+  def light_cast(pid, num, order) do
+    IO.inspect(order, label: "REQUEST ORDER: ")
+    MyGenserver.cast(pid, {:light, num, order})
   end
+
+  def get_state(pid) do
+    MyGenserver.call(pid, :get_state)
+  end
+
+  def stop_server(pid, reason \\ :normal) do
+    MyGenserver.stop(pid, reason)
+  end
+
+  # CALLBACKS
 
   def init(:ok) do
     {:ok, 0}
   end
 
-  def handle_call(:show, _from, state) do
+  def handle_call(:get_state, _from, state) do
     {:reply, state, state}
   end
 
-  def handle_cast({:expensive, num}, state) do
+  def handle_call({:expensive, num, order}, _from, state) do
+    IO.inspect(order, label: "PROCESS ORDER: ")
+    expensive_function()
+    {:reply, :ok, state + num}
+  end
+
+  def handle_call({:light, num, order}, _from, state) do
+    IO.inspect(order, label: "PROCESS ORDER: ")
+    {:reply, :ok, state + num}
+  end
+
+  def handle_cast({:expensive, num, order}, state) do
+    IO.inspect(order, label: "PROCESS ORDER: ")
     expensive_function()
     {:noreply, state + num}
   end
 
-  def handle_cast({:light, num}, state) do
+  def handle_cast({:light, num, order}, state) do
+    IO.inspect(order, label: "PROCESS ORDER: ")
     {:noreply, state + num}
   end
 
-  def handle_info({:add, num}, state) do
+  def handle_info({:expensive, num, order}, state) do
+    IO.inspect(order, label: "PROCESS ORDER: ")
+    expensive_function()
+    {:noreply, state + num}
+  end
+
+  def handle_info({:light, num, order}, state) do
+    IO.inspect(order, label: "PROCESS ORDER: ")
     {:noreply, state + num}
   end
 
   def terminate(reason, _state) do
     IO.inspect(reason, label: "Terminate Reason: ")
   end
+
+  # IMPL
 
   defp expensive_function do
     Enum.map(1..10_000_000, &(&1 * &1))
--- test/example_test.exs
+++ test/example_test.exs
@@ -1,21 +1,53 @@
 defmodule ExampleTest do
   use ExUnit.Case
 
-  test "greets the world" do
+  test "handle_call" do
+    {:ok, pid} = Example.start_link()
+
+    Example.expensive_call(pid, 1, 1)
+    Example.light_call(pid, 2, 2)
+
+    assert Example.get_state(pid) == 3
+  end
+
+  test "handle_cast" do
+    {:ok, pid} = Example.start_link()
+
+    Example.expensive_cast(pid, 1, 1)
+    Example.light_cast(pid, 2, 2)
+
+    assert Example.get_state(pid) == 3
+  end
+
+  test "handle_info" do
+    {:ok, pid} = Example.start_link()
+
+    send(pid, {:expensive, 1, 1})
+    send(pid, {:light, 2, 2})
+
+    assert Example.get_state(pid) == 3
+  end
+
+  test "stop" do
     {:ok, pid} = Example.start_link()
     Process.monitor(pid)
-
-    Example.expensive(pid, 1)
-    assert Example.show(pid) == 1
-    Example.light(pid, 1)
-
-    assert Example.show(pid) == 2
-    send(pid, {:add, 1})
-    assert Example.show(pid) == 3
+    reason = :custom_reason
 
     assert Process.alive?(pid)
-    Example.stop_server(pid)
-    assert_receive {:DOWN, _, _, _, reason}
+    Example.stop_server(pid, reason)
+    assert_receive {:DOWN, _, _, _, ^reason}
     refute Process.alive?(pid)
+  end
+
+  test "combine callbacks" do
+    {:ok, pid} = Example.start_link()
+    Example.expensive_call(pid, 1, 1)
+    Example.expensive_cast(pid, 2, 2)
+    send(pid, {:expensive, 3, 3})
+    send(pid, {:light, 4, 4})
+    Example.light_cast(pid, 5, 5)
+    Example.light_call(pid, 6, 6)
+
+    assert Example.get_state(pid) == 21
   end
 end
